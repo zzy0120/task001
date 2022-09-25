8fd03b373386d5018e91b0b991886f9660ba810d
zzy0120 <1481436865@qq.com>
2022-09-25 10:23:27 +0800

    Initial commit

---
+++ day001/src/com/zzy/all/hello.java
@@ -0,0 +1,16 @@
+package com.zzy.all;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("123");
+        List<String> list = new ArrayList<String>();
+        for(String s:list){
+        }
+
+
+
+    }
+}
